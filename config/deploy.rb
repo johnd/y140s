72cfbcc4b05238df9df988fadfbb69dd1d6d9608
John Daniels <john@semantici.st>
2010-02-28 17:52:33 +0000

    Clean up, initial deploy testing.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,7 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "y140s"
+set :repository,  "/Users/john/Code/y140s/.git"
+set :deploy_to, "/home/deploy/live"
+set :deploy_via, :copy
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
@@ -10,6 +12,30 @@ set :repository,  "set your repository location here"
 # your SCM below:
 # set :scm, :subversion
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+set :scm, :git
+set :user, "deploy"
+set :app_server, :passenger
+set :use_sudo, false
+set :domain, 'y140s.com'
+
+
+# Allow ssh to use ssh keys
+set :ssh_options, { :forward_agent => true }
+
+role :app, domain
+role :web, domain
+
+deploy.task :restart do
+  # Restart Passenger
+  run "touch #{current_path}/webapp/tmp/restart.txt"
+  # Restart Adhearsion
+  run "ahnctl restart #{current_path}/telapp"
+end
+
+deploy.task :symlinks do
+  run "ln -nfs #{shared_path}/config/config.yml #{release_path}/telapp/config/config.yml"
+end
+
+
+after :deploy, 'deploy:cleanup'
+after 'deploy:update_code', 'deploy:symlinks'
